016413e30f5f97b257d0a3f21b038c44d57aa1e0
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-02-15 11:03:13 +0100

    Add files via upload

---
+++ CircleTest.java
@@ -0,0 +1,41 @@
+package es.uah.matcomp.mp.e1.ejerciciosclases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CircleTest {
+
+    @Test
+    void getRadius() {
+        Circle j = new Circle();
+        assertEquals(1.1, j.getRadius(), 1.1);
+    }
+
+    @Test
+    void setRadius() {
+        Circle j = new Circle();
+        assertDoesNotThrow(()->j.setRadius(1.1));
+        assertEquals(1.1,j.getRadius(),"Los radios no coinciden.");
+    }
+
+
+    @Test
+    void getArea() {
+        Circle j = new Circle();
+        assertEquals(Math.PI, j.getArea(), 1.1);
+
+    }
+
+    @Test
+    void getCircunference() {
+        Circle j = new Circle(1.1);
+        assertEquals(Math.PI*2, j.getCircunference(), 1.0);
+    }
+
+    @Test
+    void testToString() {
+        Circle j = new Circle();
+        assertEquals("Circle[radius="+1.0+"]", j.toString(), "No funciona correctamente");
+    }
+}
