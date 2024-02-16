bb9b45300aa1ffbd539d1d95707e032d729389b0
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-02-16 13:50:43 +0100

    Circulo Terminado y Rectangulo en proceso :)

---
+++ test/java/es/uah/matcomp/mp/el1/ejercicios/RectangleTest.java
@@ -0,0 +1,36 @@
+package es.uah.matcomp.mp.el1.ejercicios;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    @Test
+    void getLength() {
+    }
+
+    @Test
+    void setLength() {
+    }
+
+    @Test
+    void getWidth() {
+    }
+
+    @Test
+    void setWidth() {
+    }
+
+    @Test
+    void getArea() {
+    }
+
+    @Test
+    void getPerimeter() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
