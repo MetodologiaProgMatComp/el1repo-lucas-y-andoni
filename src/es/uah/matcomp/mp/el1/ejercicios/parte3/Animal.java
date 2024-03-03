a32b1219b9ec61a58dbed22332542e992a2a8992
LucasMarcosVaquerizo <157593171+LucasMarcosVaquerizo@users.noreply.github.com>
2024-03-03 19:44:10 +0100

    parte tres terminada hasta las clases Shape y subclases correspondientes.

---
+++ src/es/uah/matcomp/mp/el1/ejercicios/parte3/Animal.java
@@ -0,0 +1,11 @@
+package es.uah.matcomp.mp.el1.ejercicios.parte3;
+
+public class Animal {
+    private String name;
+    public Animal(String name) {
+        this.name = name;
+    }
+    public String toString() {
+        return "Animal[name= " + name + "]";
+    }
+}
